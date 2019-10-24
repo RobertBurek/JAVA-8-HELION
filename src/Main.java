a5a7ee881ed9f998ae0513b2fed3edf85da2df97
Robert Burek <robertburek@wp.pl>
2019-10-24 22:40:11 +0200

    INSTRUKCJE STERUJĄCE

--- src/Main.java
+++ src/Main.java
@@ -1,86 +1,80 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 /**
  * Created by Robert Burek
  */
 public class Main {
     public static void main(String[] args) {
 
-//        ZMIENNE, STAŁE I NAZEWNICTWO
-//        1)
-//        int myInt;
-//        myInt = 10;
-//        System.out.println("myInt = " + myInt);
-//
-//        String myString;
-//        myString = "mój string";
-//        System.out.println("myString = " + myString);
-//
-//        final int myFinalInt = 11;
-////        myFinalInt = 12;  nie można zmienić stałej
-//        System.out.println("myFinalInt = " + myFinalInt);
+//        Instrukcje sterujące
+
+//        1) IF
+//        int number = 3;
+//        if (number % 2 == 0) {
+//            System.out.println("Liczba = " + number + ", parzysta");
+//        } else if (number % 3 == 0) {
+//            System.out.println("Liczba = " + number + ", nieparzysta i podzielna przez 3");
+//        } else {
+//            System.out.println("Liczba = " + number + ", nieparzysta");
+//        }
 
 
-//        2)
-//        int intInMain = 30;
-//        System.out.println("intInMain (main) = " + intInMain);
-//        System.out.println("MAX_NUMBER_OF_STH (main) = " + MAX_NUMBER_OF_STH);
-//        System.out.println("maxNumber (main) = " + maxNumber);
-//        method();
+//        2) IF cd
+//        int number = 15;
+//        if (number % 2 == 0 && number > 0) {
+//            System.out.println("Liczba = " + number + ", parzysta i większa od zera");
+//        } else if (number % 3 == 0 || number % 5 == 0) {
+//            System.out.println("Liczba = " + number + ", nieparzysta i podzielna przez 3 lub 5");
+//        } else {
+//            System.out.println("Liczba = " + number + ", nieparzysta");
+//        }
+//        && ||  &  >,<.==,
 
 
-//        3) Nazewnictwo
-//            int zmiennaWMetodzie = 10;
+//        3) equals i equalsIgnoreCase
+//        String imie = "ala";
+//        if (imie.equals("Ala")) {
+//            System.out.println("imie = " + imie);
+//        } else if (imie.equalsIgnoreCase("Ala")) {
+//            System.out.println("Jednak imie = " + imie);
+//        }
+
+//        4) switch
+          int monthNumber = 3;
+          String monthName = "";
+          switch (monthNumber){
+              case 1:
+                  monthName = "Styczeń";
+                  System.out.println("monthName = " + monthName);
+                  break;
+              case 2:
+                  monthName = "Luty";
+                  System.out.println("monthName = " + monthName);
+                  break;
+              case 3:
+              case 4:
+                  monthName = "Marzec lub Kwiecień";
+                  System.out.println("monthName = " + monthName);
+                  break;
+              default:
+                  System.out.println("Nie ma takiego miesiąca");
+                  break;
+          }
+          switch (monthName){
+              case "Styczeń":
+                  System.out.println("monthNumber = 1");
+                  break;
+              case "Luty":
+                  System.out.println("monthNumber = 2");
+                  break;
+              case "Marzec lub Kwiecień":
+                  System.out.println("monthNumber = 3 lub 4");
+                  break;
+              default:
+                  System.out.println("Inny numer miesiąca");
+                  break;
+          }
 
 
-//        4) Rzutowanie na prymitywach
-//        int a = 8;
-//        int b = 3;
-//        int c = a / b;
-//        System.out.println("c = " + c);
-//        double d = (double) (a / b);
-//        System.out.println("d = " + d);
-//        double e = a / (double) b;
-//        System.out.println("e = " + e);
-
-
-//        5) Maszyna do kawy V1
-        int iloscKaw = 0;
-
-        // pobranie liczby kaw od użytkownika
-        System.out.print("Podaj liczbe kaw: ");
-        Scanner interfejsUzytkownika = new Scanner(System.in);
-        try {
-            iloscKaw = interfejsUzytkownika.nextInt();
-        } catch (InputMismatchException ex) {
-            System.out.println("Błędne dane!!!");
-        }
-        System.out.println("Zamówiono " + iloscKaw + " kaw.");
 
     }
-
-
-//    2)
-//    static final int MAX_NUMBER_OF_STH = 10;
-//    static final int maxNumber = 20;
-//
-//
-//    private static void method(){
-////        System.out.println("intInMain (main) = " + intInMain);  zmienna lokalna
-//        System.out.println("MAX_NUMBER_OF_STH (method) = " + MAX_NUMBER_OF_STH);
-//        System.out.println("maxNumber (method) = " + maxNumber);
-//
-//    }
-
-
-//    3)
-//    static final int NAZWA_STALEJ_NOWA = 10;
-//    static int poleStatyczne = 10;
-//    int poleKlasy = 10;
-//
-//    void nazwaMetodyOpisujacaCoRobi(int nazwaArgumentu) {
-//        int zmiennaWMetodzie = 10;
-//    }
 
 }
